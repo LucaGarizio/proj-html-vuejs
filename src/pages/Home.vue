d79bf140c8a70d066fd422e8c5fe5928c56ddfca
Luca Garizio <garizioluca2@gmail.com>
2024-01-15 21:41:53 +0100

    style: add first structure third section home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -10,12 +10,38 @@ export default {
 
     <!-- luca -->
 
-    <section>
-        <h1>ciaooo</h1>
+    <section class="my-3">
+        <div class="my container-fluid">
+            <div class="row d-flex justify-content-center">
+                <div class="l col-4 px-5 text-center">
+                    <h2 class="fs-1 mb-3">
+                        I coach and mentor visual artists, like you, to Create
+                        Like you Mean It
+
+                        <h4>Martin Garrix</h4>
+                    </h2>
+                </div>
+                <div class="l col-5 offset-1">
+                    <p>
+                        As a coach and mentor, my ultimate goal is to motivate
+                        visual artists toward inspired action. I help you get
+                        you back on track to being the productive, creative,
+                        meaningful and authentic artist you truly want to be.I
+                        support artists in crafting and living a creative life
+                        as well as feeling good about themselves and their art.
+                        <br />
+                        <br />
+                        As an artist and an educator for nearly 30 years, I
+                        understand what it takes to create.
+                    </p>
+                    <button>Get started today</button>
+                </div>
+            </div>
+        </div>
     </section>
-    <section></section>
-    <section></section>
-    <section></section>
+    <section class="my-3"></section>
+    <section class="my-3"></section>
+    <section class="my-3"></section>
 
     <!-- dome -->
     <section></section>
@@ -25,4 +51,19 @@ export default {
 <style lang="scss" scoped>
 @use '../styles/partials/variables' as *;
 @use '../styles/partials/mixins' as *;
+
+// STILI ALE
+
+// STILI LUCA
+.my,
+.container {
+    border: 1px solid black;
+}
+
+.l,
+.col-4 {
+    border: 1px solid black;
+}
+
+// STILI DOME
 </style>
