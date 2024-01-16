961001fb9f67598e3c191dd6e1c6407f040426a5
Dome1992 <forlanodomenico92@mail.com>
2024-01-16 10:21:51 +0100

    feat - setup

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -44,7 +44,73 @@ export default {
     <section class="my-3"></section>
 
     <!-- dome -->
-    <section></section>
+    <section class="my-3">
+        <div>
+    
+            <div class="text-center mt-5">
+                <!-- La classe "text-center" allinea il testo al centro -->
+                <h2>Article and Tips</h2>
+                <h3>Latest From The Blog</h3>
+            </div>
+
+        </div>
+
+        <div class="container">
+  
+
+            <div class="row">
+            <!-- Utilizza una row Bootstrap per organizzare le card -->
+
+                <div class="col-md-4">
+                <!-- La classe "col-md-4" indica che la colonna occupa 4 unità su 12 su schermi di dimensioni medie e superiori -->
+                    <div class="card">
+                        <!-- Aggiungi la classe "card" per creare una card Bootstrap -->
+
+                        <img src="" class="card-img-top" alt="Immagine 1">
+                        <!-- La classe "card-img-top" allinea l'immagine nella parte superiore della card -->
+
+                        <div class="card-body">
+                        <!-- Aggiungi la classe "card-body" per il corpo della card -->
+
+                        <p class="card-text">Contenuto Card 1</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                <!-- La classe "col-md-8" indica che la colonna occupa 8 unità su 12 su schermi di dimensioni medie e superiori -->
+                    <div class="card">
+                        <!-- Aggiungi la classe "card" per creare una card Bootstrap -->
+
+                        <img src="" class="card-img-top" alt="Immagine 2">
+                        <!-- La classe "card-img-top" allinea l'immagine nella parte superiore della card -->
+
+                        <div class="card-body">
+                        <!-- Aggiungi la classe "card-body" per il corpo della card -->
+
+                        <p class="card-text">Contenuto Card Centrale</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                <!-- La classe "col-md-4" indica che la colonna occupa 4 unità su 12 su schermi di dimensioni medie e superiori -->
+                    <div class="card">
+                        <!-- Aggiungi la classe "card" per creare una card Bootstrap -->
+
+                        <img src="" class="card-img-top" alt="Immagine 3">
+                        <!-- La classe "card-img-top" allinea l'immagine nella parte superiore della card -->
+
+                        <div class="card-body">
+                        <!-- Aggiungi la classe "card-body" per il corpo della card -->
+
+                        <p class="card-text">Contenuto Card 3</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
     <section></section>
 </template>
 
@@ -66,4 +132,5 @@ export default {
 }
 
 // STILI DOME
+
 </style>
