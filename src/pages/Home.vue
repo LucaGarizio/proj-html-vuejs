73b92b6d352dc058d36e9c5d31082a19ad33ee18
Dome1992 <forlanodomenico92@mail.com>
2024-01-18 19:13:46 +0100

    section dinamica dome

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -717,9 +717,12 @@ export default {
         
         
 
-        <div class="container">
+        <div class="container" id="section-upcoming">
 
-
+            <div>
+                <img src="../assets/images/maxcoach-shape-13.png" class="maxcoach-section-upcoming" alt="">
+                <img src="../assets/images/maxcoach-shape-05-150x150.png" class="maxcoach-shape-section-upcoming" alt="">
+            </div>
             <div class="text-center mt-5">
                 <!-- La classe "text-center" allinea il testo al centro -->
                 <h2 class="cursive">Upcoming Event</h2>
@@ -1396,6 +1399,38 @@ export default {
 .span-orange:hover {
     border-bottom: 1px solid;
 }
+// sezione UpComing
+.container{
+    position: relative;
+}
+
+.maxcoach-section-upcoming{
+    position: absolute;
+    top: 35%;
+    left: 87%;
+    z-index:2;
+
+    transition: transform 1s;
+}
+.maxcoach-shape-section-upcoming{
+    position: absolute;
+    top: 0%;
+    right: 92%;
+
+    transition: transform 1s;
+
+}
+.container:hover{
+  .maxcoach-section-upcoming{
+    transform: translateX(40px) translateY(-40px);
+  }
+
+  .maxcoach-shape-section-upcoming{
+    transform: translateX(40px) translateY(-40px);
+  }
+}
+
+
 
 #descrizione-col8 {
     align-items: center;
